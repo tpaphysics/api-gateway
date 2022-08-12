0321e5107390fba748dda98df5902774a5b5d65b
tpaphysics <physics.thiago@gmail.com>
2022-08-12 17:12:28 -0300

    feat: criação do crud users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class User {
+  @ApiProperty()
+  id?: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  created_at?: string;
+
+  @ApiProperty()
+  updated_at?: string;
+}
